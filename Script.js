0128120fc5986a4c68b757a46a7d8fe3928963f9
Ivan <Ivanv257@gmail.com>
2018-01-07 14:21:18 +0200

    External Javascript file created

---
+++ Script.js
@@ -0,0 +1,17 @@
+/* Pop Up Window using if else conditions*/
+let myPopUpWindow = true;
+
+if (myPopUpWindow === true){
+  window.alert("Welcome to Ivan's Solution for Google");
+} else {
+  window.alert("The statement is false");
+}
+
+/* Message inserted into the console log using if else conditions*/
+let myConsolePrint = true;
+
+if (myConsolePrint = 1){
+console.log("This is Ivan's solution for Google");
+} else {
+console.log("The statement returned false");
+}
